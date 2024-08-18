adec1f46e40a92149d82b16f5ecf49007c324db8
Ndzi Nfor <ndzinfor@gmail.com>
2024-08-18 19:57:54 +0100

    write set(key, value) function

--- HasMap.js
+++ HasMap.js
@@ -1,7 +1,7 @@
 class Hashmap {
     constructor(size = 16){
         this.size = size; // Size of the hash table
-        this.table = new Array(this.size); // Array to hold the buckets
+        this.bucket = new Array(this.size); // Array to hold the buckets
     }
     // hash function takes a key and produces a hash code
     hash(key) {
@@ -14,6 +14,22 @@ class Hashmap {
      
         return hashCode;
     } 
+
+    set(key, value) {
+        const index = this.hash(key);
+        if (!this.table[index]) {
+            this.table[index] = []; // Create a bucket if it doesn't exist
+        }
+        // Check if the key already exists, and update the value
+        const bucket = this.table[index];
+        for (let i = 0; i < bucket.length; i++) {
+            if (bucket[i][0] === key) {
+                bucket[i][1] = value; // Update existing key
+                return;
+            }
+        }
+        bucket.push([key, value]); // Add new key-value pair
+    }
 
 
 
